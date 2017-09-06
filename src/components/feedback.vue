b61681b57a67b30be2c8693bcf381eda1fb5082d
SF-Zhou <sfzhou.scut@gmail.com>
2017-09-06 14:08:32 +0800

    tried to fix repo setting of gitalk

--- src/components/feedback.vue
+++ src/components/feedback.vue
@@ -20,7 +20,7 @@
           const gitalk = new Gitalk({
             clientID: '49aeab30501e804c71b0',
             clientSecret: '6d3c8320ac0c5c461afc2c01994833f05056b0de',
-            repo: 'https://github.com/sf-zhou/sf-zhou.github.io',
+            repo: 'sf-zhou.github.io',
             owner: 'SF-Zhou',
             admin: ['SF-Zhou'],
             distractionFreeMode: false
