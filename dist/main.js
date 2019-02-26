a26ff637c6d92d9c8399752205294acfca3cc40e
SF-Zhou <sfzhou.scut@gmail.com>
2019-02-26 22:42:05 +0800

    Hide hidden gitalk comment

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,13 @@
-if (window.location.pathname.endsWith('html')) {
+const pathname = window.location.pathname;
+
+if (pathname.endsWith('html') && pathname.indexOf("/hidden/") === -1) {
   const gitalk = new Gitalk({
     clientID: '49aeab30501e804c71b0',
     clientSecret: '6d3c8320ac0c5c461afc2c01994833f05056b0de',
     repo: 'sf-zhou.github.io',
     owner: 'SF-Zhou',
     admin: ['SF-Zhou'],
-    id: window.location.pathname,
+    id: pathname,
   });
 
   gitalk.render('gitalk-container');
