0673ed5147ddae83cb1841aafbf5f2e15e444fcc
SF-Zhou <sfzhou.scut@gmail.com>
2019-07-15 10:26:19 +0800

    Add about page and WeChat reward code

--- dist/main.js
+++ dist/main.js
@@ -12,3 +12,6 @@ if (pathname.endsWith('html') && pathname.indexOf("/hidden/") === -1) {
 
   gitalk.render('gitalk-container');
 }
+
+// Google Analytics
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-61723712-2', 'auto'); ga('send', 'pageview');
