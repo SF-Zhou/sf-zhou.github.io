9ab3b0683456e174cae804c887e40ee8781cc849
SF-Zhou <sfzhou.scut@gmail.com>
2019-10-12 14:51:19 +0800

    Update author description && Add tag filter

--- dist/main.js
+++ dist/main.js
@@ -13,5 +13,30 @@ if (pathname.endsWith('html') && pathname.indexOf("/hidden/") === -1) {
   gitalk.render('gitalk-container');
 }
 
+if (pathname === "/" || pathname === "/index.html") {
+  window.onhashchange = function () {
+    const mark = window.location.hash.substr(2);
+    const cards = document.getElementsByClassName("card");
+    for (card of cards) {
+      var found = false;
+      const tags = card.getElementsByClassName("tag");
+      for (tag of tags) {
+        const a = tag.children[0];
+        if (a.innerText === mark) {
+          tag.classList.add("selected");
+          a.href = "#";
+          found = true;
+        } else {
+          tag.classList.remove("selected");
+          a.href = "#/" + a.innerText;
+        }
+      }
+      card.hidden = mark.length > 0 && !found;
+    }
+  }
+
+  window.onhashchange();
+}
+
 // Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); ga('create', 'UA-61723712-2', 'auto'); ga('send', 'pageview');
