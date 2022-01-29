7a72d17605c208bf77afaf9e51b7c43abc6ecde1
SF-Zhou <sfzhou.scut@gmail.com>
2022-01-29 02:11:47 +0000

    Deploying to master from @ SF-Zhou/sf-zhou.github.io@1946eec0e96e6fc16f239f200679c54a5565a3dc 🚀

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,5 @@
 const pathname = window.location.pathname;
 
-if (pathname.endsWith('html')) {
-  const gitalk = new Gitalk({
-    clientID: '49aeab30501e804c71b0',
-    clientSecret: '6d3c8320ac0c5c461afc2c01994833f05056b0de',
-    repo: 'sf-zhou.github.io',
-    owner: 'SF-Zhou',
-    admin: ['SF-Zhou'],
-    id: pathname,
-  });
-
-  gitalk.render('gitalk-container');
-}
-
 if (pathname === "/" || pathname === "/index.html") {
   window.onhashchange = function () {
     const mark = window.location.hash.substr(2);
